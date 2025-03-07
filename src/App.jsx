1513e3e5fb534b3afc9f0fbbb63b42b78209efb4
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2025-03-07 06:26:35 +0530

    projectmanagement1

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,38 @@ import Newproject from './components/Newproject';
 import NoProjectSelected from './components/NoProjectSelected';
 
 function App() {
+
+  const [ProjectsState, setProjectsState ] = useState({
+    selectedProjectId:undefined,
+    projects: []
+  })
+  
+  function handeleSelectProjects () {
+    setProjectsState (prevState => {
+      return{
+        ...prevState,
+        selectedProjectId: null, 
+      };
+    });
+  }
+  
+ let content;
+ 
+ if(ProjectsState.selectedProjectId === null){
+  content = <Newproject/>
+ }else if (ProjectsState.selectedProjectId === undefined) {
+  content = <NoProjectSelected onStartAddProject= {handeleSelectProjects}/>;
+ }
+  
   const [count, setCount] = useState(0)
+
 
   return (
     <>
       <main className='h-screen my-8 flex gap-8'>
-        <Projectsidebar/>
-        <NoProjectSelected/>
-
+        <Projectsidebar onStartAddProject= {handeleSelectProjects}/>
+        
+        {content}
       </main>
     </>
   )
--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,13 +1,13 @@
 import projectImage from '../assets/project-1.png'
 import Button from './Button'
-function NoProjectSelected () {
+function NoProjectSelected ( onStartAddProject) {
 return (
     <div className='mt-24 text-center w-2/3'>
         <img src={projectImage} alt="empty task list" className='w-16 h-16 object-contain mx-auto'/>
         <h2 className='text-xl font-bold text-stone-500 my-4'>No project Selected </h2>
         <p className='text-stone-400 mb-4'>select a project or get stared with new one </p>
         <p className='mt-8'>
-            <Button>create new Project</Button>
+            <Button onClick={onStartAddProject}>create new Project</Button>
         </p>
     </div>
 )
