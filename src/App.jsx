bf4f100f2bd8ba14481e0d22faecaa957aa91c92
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2025-03-08 08:13:11 +0530

    tasksn

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './index.css';
 import Projectsidebar from './components/Projectsidebar';
 import Newproject from './components/Newproject';
 import NoProjectSelected from './components/NoProjectSelected';
+import SelectedProject from './components/SelectedProject';
 
 function App() {
   const [projectsState, setProjectsState] = useState({
@@ -10,40 +11,58 @@ function App() {
     projects: [],
   });
 
-  function handleSelectProjects() {
+  function handleSelectProject(id) {
+    setProjectsState((prevState) => ({
+      ...prevState,
+      selectedProjectId: id,
+    }));
+  }
+
+  function handleStartAddProject() {
     setProjectsState((prevState) => ({
       ...prevState,
       selectedProjectId: null,
     }));
   }
 
- function handleAddProject ( projectData) {
-  setProjectsState(prevState => {
-    const projectId = Math.random();
-    const newProject = {
-      ...projectData,
-      id: projectId,
-    }
-    return {
-      ...prevState,
-      selectedProjectId:undefined,
-      projects: [...prevState.projects,newProject]
-    }
-  });
- } 
+  function handleAddProject(projectData) {
+    setProjectsState((prevState) => {
+      const projectId = Math.random().toString(); // Ensure ID is unique
+      const newProject = {
+        ...projectData,
+        id: projectId,
+      };
+
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  const selectedProject = projectsState.projects.find(
+    (project) => project.id === projectsState.selectedProjectId
+  );
 
   let content;
-
   if (projectsState.selectedProjectId === null) {
     content = <Newproject onAdd={handleAddProject} />;
-  } else if (projectsState.selectedProjectId === undefined) {
-    content = <NoProjectSelected onStartAddProject={handleSelectProjects} />;
+  } else if (selectedProject) {
+    content = <SelectedProject project={selectedProject} />;
+  } else {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
 
   return (
     <>
       <main className="h-screen my-8 flex gap-8">
-        <Projectsidebar onStartAddProject={handleSelectProjects} projects={projectsState.projects}/>
+        <Projectsidebar
+          onSelectProject={handleSelectProject}
+          onStartAddProject={handleStartAddProject}
+          projects={projectsState.projects}
+          selectedProjectId={projectsState.selectedProjectId}
+        />
         {content}
       </main>
     </>
